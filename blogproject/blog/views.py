214e463859d0a953109002723171cfe3c3ea9e07
uru96 <w.uruski96@gmail.com>
2021-10-24 16:55:47 +0200

    adding forms

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages
-                               )
-    return render(request, 'blog/post/list.html',
-                            {'page': page,
-                             'posts': posts})
+# Showing posts on main page
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
+# Showing post details
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
@@ -26,3 +22,28 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+# Sharing posts
+def post_share(request, post_id):
+    email_from = 'uru1996@gmail.com'
+    # Get post based on ID
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Veryfing post is success
+            cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = '{} ({}) Zachęca do przeczytania "{}"'.format(cd['name'], ['email'], post.title)
+            message = 'Przeczytaj post "{}" na stronie {}\n \n Komentarz dodany przez {}: {} '.format(post.title,
+                                                                                                      post_url,
+                                                                                                      cd['name'],
+                                                                                                      cd['comments'])
+            send_mail(subject, message, email_from, [cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 
 # Application definition
 # Informs Django about active applications
@@ -127,3 +134,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
