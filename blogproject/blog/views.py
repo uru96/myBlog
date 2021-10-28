99f1ffd3c3e5ac2369aae87daf09699603ddea60
uru96 <w.uruski96@gmail.com>
2021-10-28 18:21:40 +0200

    added tags

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
-from django.views.generic import ListView
+#from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 # Showing posts on main page
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 5
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 5
+#     template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
+
 
 
 # Showing post details
