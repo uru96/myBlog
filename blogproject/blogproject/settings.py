271210a85fe5128bf2e43307316a2aed18d98799
uru96 <w.uruski96@gmail.com>
2021-11-07 16:31:36 +0100

    deploy to heroku v2

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-!r(=yhx61a)va5@@77hdb7yj@$f01_g89xj-oux&vriwju80&*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djangoblogaproject.herokuapp.com/',
+                 'localhost',
+                 '127.0.0.1']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
